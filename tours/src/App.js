451a6ce8cf3d6975ff9c8b9a4c438182aaa3ed76
Edy Garcia <edygarcia@edys-imac.attlocal.net>
2019-07-24 13:32:16 -0400

    initialized

--- tours/src/App.js
+++ tours/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import "./App.scss"
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1> Hllo </h1>
+        <h1> Testing App</h1>
 
       </div>
     );
