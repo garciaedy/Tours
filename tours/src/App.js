f3421c38cb829aed698174e9fd5e293064caf6da
Edy Garcia <edygarcia@edys-mbp.attlocal.net>
2019-08-12 21:14:43 -0400

    new commit

--- tours/src/App.js
+++ tours/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import "./App.scss"
+import Navbar from './components/Navbar'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1> Testing App</h1>
+
+       <Navbar/>
 
       </div>
     );
